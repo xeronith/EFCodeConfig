9ed1a94fcaedcbdc76663f7a09c80ff47cb34c77
Xeronith <xeronith@yahoo.com>
2016-06-09 08:39:09 +0430

    //

---
+++ EFCodeConfig/Main.cs
@@ -0,0 +1,40 @@
+﻿using EFCodeConfig.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFCodeConfig
+{
+    public partial class Main : Form
+    {
+        Model model = new Model();
+
+        public Main()
+        {
+            InitializeComponent();
+
+            model.Categories.Load();
+            categoryBindingSource.DataSource = model.Categories.Local.ToBindingList();
+        }
+
+        private void categoryBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            Validate();
+            model.SaveChanges();
+            categoryDataGridView.Refresh();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            model.Dispose();
+        }
+    }
+}
